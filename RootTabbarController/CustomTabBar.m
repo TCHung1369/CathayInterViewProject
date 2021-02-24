35dc8e7e5b19b6a268f60d69aae17ab763007e3e
TCHung1369 <snowcircle20217@gmail.com>
2021-02-24 15:06:29 +0800

    Modify bug.

--- RootTabbarController/CustomTabBar.m
+++ RootTabbarController/CustomTabBar.m
@@ -14,13 +14,6 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-//        self.KOButton = [UIButton buttonWithType:UIButtonTypeCustom];
-//
-//        [self.KOButton setBackgroundImage:[UIImage imageNamed:@"icTabbarHomeOff"] forState:UIControlStateNormal];
-//
-//        [self.KOButton addTarget:self action:@selector(KOButtonClick) forControlEvents:UIControlEventTouchUpInside];
-//        [self addSubview:self.KOButton];
-        //rgb 236 0 140
         [self setTintColor:[UIColor colorWithRed:236.0/255.0 green:0.0 blue:140.0/255.0 alpha:1.0]];
         [self setBackgroundColor:[UIColor clearColor]];
         [self setBackgroundImage:[UIImage imageNamed:@"imgTabbarBg"]];
@@ -49,8 +42,6 @@
     for (UIView * barButton in [self subviews]) {
         if ([barButton isKindOfClass:NSClassFromString(@"UITabBarButton")]){
             if(barButtonIndex == 2){
-//                [self.KOButton setFrame:CGRectMake(0.0, 0.0, self.KOButton.currentImage.size.width, self.KOButton.currentImage.size.height)];
-//                [self.KOButton setCenter:CGPointMake(self.center.x, 34)];
                 barButtonIndex += 1;
             }
             [barButton setFrame:CGRectMake(barButtonWidth * barButtonIndex, 11, barButtonWidth, self.frame.size.height)];
@@ -61,24 +52,6 @@
     
     [self bringSubviewToFront:self.KOButton];
 }
-
-//-(UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event{
-//
-//    if (self.isHidden) {
-//        return [super hitTest:point withEvent:event];
-//    }else {
-//        CGPoint onButton = [self convertPoint:point toView:self.KOButton];
-//        if ([self.KOButton pointInside:onButton withEvent:event]) {
-//            return self.KOButton;
-//        }else{
-//            return [super hitTest:point withEvent:event];
-//        }
-//
-//    }
-//
-//
-//}
-
 
 -(CGSize)sizeThatFits:(CGSize)size
 {
