1b5d3760834ae7e5016bb0a611bd71e87a932547
Tzu Chen Hung <snowcircle20217@gmail.com>
2021-07-26 10:00:13 +0800

    addobject for test

---
+++ FriendListTableView/TestObject.h
@@ -0,0 +1,17 @@
+//
+//  TestObject.h
+//  CathayInterViewProject
+//
+//  Created by Tzu Chen Hung on 2021/7/26.
+//  Copyright © 2021 Tzu-Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FriendListTableView/TestObject.m
@@ -0,0 +1,13 @@
+//
+//  TestObject.m
+//  CathayInterViewProject
+//
+//  Created by Tzu Chen Hung on 2021/7/26.
+//  Copyright © 2021 Tzu-Chen. All rights reserved.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
+@end
